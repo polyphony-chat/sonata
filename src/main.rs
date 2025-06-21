934fbeb22d5e43c4e09fba7f92ea17a4f4547692
bitfl0wer <florian@pro-weber.com>
2025-06-21 21:46:48 +0200

    chore: add crate docs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+/*!
+ * # sonata
+ *
+ * A robust, performant polyproto home server.
+ */
+
 mod api;
 pub(crate) mod cli;
 pub(crate) mod config;
