f09bf84d9d9ddb7d7110d50c3cfeb82042ece676
bitfl0wer <florian@pro-weber.com>
2025-06-21 18:45:48 +0200

    feat: add services.sql

---
+++ migrations/0004_services.sql
@@ -0,0 +1,11 @@
+CREATE TYPE service AS (
+    service_id VARCHAR (64),
+    service_url TEXT,
+    is_primary BOOLEAN
+);
+
+CREATE TABLE IF NOT EXISTS actor_service_mapping (
+    id BIGSERIAL PRIMARY KEY,
+    uaid UUID UNIQUE REFERENCES actors (uaid) ON DELETE CASCADE,
+    services SERVICE [] NULL
+);
