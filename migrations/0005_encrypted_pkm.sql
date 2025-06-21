7caf3a9c91453c8fccd0979ae92eb4e49da7a24e
bitfl0wer <florian@pro-weber.com>
2025-06-21 18:47:13 +0200

    feat: add encrypted pkm table

---
+++ migrations/0005_encrypted_pkm.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS encrypted_pkm (
+    id BIGSERIAL PRIMARY KEY,
+    uaid UUID REFERENCES actor (uaid) ON DELETE CASCADE,
+    key_data TEXT NOT NULL,
+    encryption_algorithms VARCHAR(255) [5] NOT NULL,
+    UNIQUE (uaid, key_data)
+);
+
+COMMENT ON TABLE encrypted_pkm IS 'Encrypted private key material, uploaded by actors';
