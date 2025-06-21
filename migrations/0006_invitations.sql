69195abf1ced3772427908e060d32110f786c7e8
bitfl0wer <florian@pro-weber.com>
2025-06-21 19:02:39 +0200

    feat: create invitations migration

---
+++ migrations/0006_invitations.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS invite_links (
+    id BIGSERIAL PRIMARY KEY,
+    invite_link_owner UUID NOT NULL REFERENCES actors (uaid) ON DELETE CASCADE,
+    usages_current INT NOT NULL DEFAULT 0,
+    usages_maximum INT NOT NULL DEFAULT 1,
+    invite VARCHAR(16) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS invitations (
+    invite_id BIGINT NOT NULL REFERENCES invite_links (id),
+    uaid_inviter UUID NOT NULL REFERENCES actors (uaid) ON DELETE CASCADE,
+    uaid_invited UUID NOT NULL REFERENCES actors (uaid) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS reputation (
+    uaid UUID REFERENCES actors (uaid) ON DELETE CASCADE PRIMARY KEY,
+    score INT NOT NULL DEFAULT 0
+);
+
+ALTER TABLE actors ADD COLUMN invites_available INT NOT NULL DEFAULT 0;
