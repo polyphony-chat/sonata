bd172ff3c114d88bd26cf0d82b3c45eddda26536
bitfl0wer <florian@pro-weber.com>
2025-06-29 19:05:21 +0200

    feat: function cleanup_expired_tokens, table comment

--- migrations/0007_api_keys.sql
+++ migrations/0007_api_keys.sql
@@ -10,3 +10,21 @@ CREATE TABLE IF NOT EXISTS user_tokens (
     uaid UUID NOT NULL REFERENCES actors (uaid) ON DELETE CASCADE,
     valid_not_after TIMESTAMP NULL
 );
+
+COMMENT ON TABLE user_tokens IS 'User access token hashes. Cleans up expired tokens on each insert operation of this table. Use view filtering to exclude expired tokens on queries.';
+
+-- We could use the postgres cron extension, but if it is possible and feasible, I'd like to stick
+-- with a "vanilla" postgres setup.
+CREATE OR REPLACE FUNCTION cleanup_expired_tokens()
+RETURNS TRIGGER AS $$
+BEGIN
+    DELETE FROM user_tokens
+    WHERE valid_not_after IS NOT NULL
+      AND valid_not_after < NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER cleanup_expired_tokens_trigger
+AFTER INSERT ON user_tokens
+EXECUTE FUNCTION cleanup_expired_tokens();
