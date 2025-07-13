d6d642db09b26a64f02252d6dae275b0ca3c080e
bitfl0wer <florian@pro-weber.com>
2025-07-13 17:33:48 +0200

    feat: PasswordRequirements trait and NISTPasswordRequirements impl

--- src/api/models/mod.rs
+++ src/api/models/mod.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 
+use crate::errors::{Context, Error};
+
 #[derive(PartialEq, Debug, Serialize, Deserialize, Clone)]
 #[serde(rename_all = "camelCase")]
 /// Information sent to the server by a client, when the client wants to create
@@ -21,4 +23,48 @@ pub struct RegisterSchema {
 	/// Optional: An invite code, which the client got referred to this instance
 	/// with.
 	pub invite: Option<String>,
+}
+
+/// A trait to verify that a password string matches a set of requirements, such
+/// as length, composition details, permitted character set, etc.
+pub trait PasswordRequirements {
+	/// Verify that a password string matches a set of requirements, such
+	/// as length, composition details, permitted character set, etc.
+	///
+	/// Returns a [String] containing the input password, if the verification
+	/// has been passed.
+	fn verify_requirements(password: &str) -> Result<String, Error>;
+}
+
+/// A very basic manifestation of NIST 2024 password security guidelines,
+/// stating:
+///
+/// - All Unicode characters are allowed, including the space (` `) character
+/// - Passwords must be at least 8 characters in length and should be at least
+///   64 characters in length (this implementation chooses 64 as a limit)
+/// - No password composition rules are enforced (Numbers, uppercase, lowercase
+///   characters are not enforced)
+///
+/// ## Warning
+///
+/// This is not a certified implementation of a NIST standard and does not claim
+/// to be one. This structs' purpose is to supply a non-frustrating set of
+/// password verification guidelines via the [PasswordRequirements] trait.
+pub struct NISTPasswordRequirements;
+
+impl PasswordRequirements for NISTPasswordRequirements {
+	fn verify_requirements(password: &str) -> Result<String, Error> {
+		let len = password.len();
+		if !(8..=64).contains(&len) {
+			return Err(Error::new(
+				crate::errors::Errcode::IllegalInput,
+				Some(Context::new(
+					Some("password"),
+					Some(&(len.to_string() + " characters")),
+					Some("More than 7 and less than 65 characters"),
+				)),
+			));
+		}
+		Ok(password.to_owned())
+	}
 }
