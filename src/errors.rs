e9edf529afb5f968c3d203d8561a37b341b63879
bitfl0wer <florian@pro-weber.com>
2025-07-13 17:33:28 +0200

    feat: Much improved Error ergonomics, docs

--- src/errors.rs
+++ src/errors.rs
@@ -16,53 +16,129 @@ pub(crate) type StdResult<T> = Result<T, StdError>;
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
+/// A polyproto core Error, with an [Errcode], an error message and optional
+/// error [Context].
+///
+/// Convenience struct to make poem-compatible and unified error returning
+/// easier
 pub struct Error {
+	/// The error code [Errcode], giving a rough idea of what went wrong
 	pub code: Errcode,
+	/// An error message, providing some further information about the category
+	/// of error encountered.
 	pub message: String,
 	#[serde(skip_serializing_if = "Option::is_none")]
 	#[serde(default)]
+	/// Optional error context.
+	///
+	/// ## Example
+	///
+	/// If a password has to be at least 8
+	/// characters long, but the user only supplied 6, the context field could
+	/// tell the user that the field `password` in their request is wrong, and
+	/// supply a very fine-grained error message, telling the user that they
+	/// only supplied 6 characters, while 8 were required.
 	pub context: Option<Context>,
 }
 
+impl Display for Error {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		f.write_str(&self.to_response_body())
+	}
+}
+
 impl Error {
-	pub fn into_response_body(self) -> String {
+	/// Performs the conversion of [Self] into a [SonataApiError].
+	pub fn into_api_error(self) -> SonataApiError {
+		SonataApiError::Error(self)
+	}
+	/// Performs the conversion of a shared reference to [Self] into JSON,
+	/// formatted as a string.
+	pub fn to_response_body(&self) -> String {
 		json!(self).to_string()
 	}
 
+	/// Creates [Self].
 	pub fn new(code: Errcode, context: Option<Context>) -> Self {
 		Self { code, message: code.message(), context }
 	}
 }
 
 #[derive(Debug, Clone, Copy, Display, Serialize, Deserialize)]
+/// Standardized polyproto core error codes, giving a rough idea of what went
+/// wrong.
 pub enum Errcode {
 	#[display("P2_CORE_INTERNAL")]
+	/// An internal error occurred.
 	Internal,
+	#[display("P2_CORE_UNAUTHORIZED")]
+	/// Unauthorized
+	Unauthorized,
+	#[display("P2_CORE_DUPLICATE")]
+	/// The resource already exists, and the context does not allow for
+	/// duplicate resources
+	Duplicate,
+	#[display("P2_CORE_ILLEGAL_INPUT")]
+	/// One or many parts of the given input did not succeed validation against
+	/// context-specific criteria
+	IllegalInput,
 }
 
 impl Errcode {
+	/// Get an error message, describing what the error code itself means.
 	pub fn message(&self) -> String {
 		match self {
-			Errcode::Internal => {
+    Errcode::Internal => {
 				"An internal error has occurred and this request cannot be processed further"
-					.to_string()
+					.to_owned()
 			}
+    Errcode::Unauthorized => {
+				"This action requires authorization, proof of which was not granted".to_owned()
+			}
+    Errcode::Duplicate => {
+				"Creation of the resource is not possible, as it already exists".to_owned()
+			}
+    Errcode::IllegalInput => "The overall input is well-formed, but one or more of the input fields fail validation criteria".to_owned(),
+            }
+	}
+}
+
+impl ResponseError for Errcode {
+	fn status(&self) -> StatusCode {
+		match self {
+			Errcode::Internal => StatusCode::INTERNAL_SERVER_ERROR,
+			Errcode::Unauthorized => StatusCode::UNAUTHORIZED,
+			Errcode::Duplicate => StatusCode::CONFLICT,
+			Errcode::IllegalInput => StatusCode::BAD_REQUEST,
 		}
 	}
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
+/// Optional error context.
+///
+/// ## Example
+///
+/// If a password has to be at least 8
+/// characters long, but the user only supplied 6, the context field could
+/// tell the user that the `field_name` "`password`" in their request is wrong,
+/// and supply a very fine-grained error message, telling the user that they
+/// only supplied 6 characters, while 8 were required.
 pub struct Context {
 	#[serde(skip_serializing_if = "String::is_empty")]
+	/// The name of the request body field which caused the error
 	pub field_name: String,
 	#[serde(skip_serializing_if = "String::is_empty")]
+	/// The value that was found to be fault inside the `field_name`
 	pub found: String,
 	#[serde(skip_serializing_if = "String::is_empty")]
+	/// The value that was expected
 	pub expected: String,
 }
 
 impl Context {
+	/// Creates [Self].
 	pub fn new(field_name: Option<&str>, found: Option<&str>, expected: Option<&str>) -> Self {
 		Self {
 			field_name: field_name.map(String::from).unwrap_or_default(),
@@ -82,7 +158,15 @@ pub(crate) enum SonataApiError {
 	StdError(StdError),
 	/// A DB-related error.
 	#[error(transparent)]
-	DbError(SonataDbError),
+	DbError(#[from] SonataDbError),
+	#[error("Error: {0}")]
+	Error(Error),
+}
+
+impl From<Error> for SonataApiError {
+	fn from(value: Error) -> Self {
+		Self::Error(value)
+	}
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -113,6 +197,7 @@ impl ResponseError for SonataApiError {
 		match self {
 			SonataApiError::StdError(_) => StatusCode::INTERNAL_SERVER_ERROR,
 			SonataApiError::DbError(sonata_db_error) => sonata_db_error.status(),
+			SonataApiError::Error(error) => error.code.status(),
 		}
 	}
 }
@@ -120,8 +205,9 @@ impl ResponseError for SonataApiError {
 impl IntoResponse for SonataApiError {
 	fn into_response(self) -> Response {
 		Response::builder().status(self.status()).body(match self {
-			SonataApiError::StdError(_) => Error::new(Errcode::Internal, None).into_response_body(),
-			SonataApiError::DbError(_) => Error::new(Errcode::Internal, None).into_response_body(),
+			SonataApiError::StdError(_) => Error::new(Errcode::Internal, None).to_response_body(),
+			SonataApiError::DbError(_) => Error::new(Errcode::Internal, None).to_response_body(),
+			SonataApiError::Error(error) => error.to_response_body(),
 		})
 	}
 }
