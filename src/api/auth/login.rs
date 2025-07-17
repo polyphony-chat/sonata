8c3cd53dc6fd2667133fe3bd2801a3a8066c6a23
bitfl0wer <florian@pro-weber.com>
2025-07-17 19:57:10 +0200

    feat: add login route!

--- src/api/auth/login.rs
+++ src/api/auth/login.rs
@@ -1,1 +1,58 @@
-// TODO
+use argon2::{Argon2, PasswordHash, PasswordVerifier};
+use log::error;
+use poem::{
+	IntoResponse, Response, handler,
+	http::StatusCode,
+	web::{Data, Json},
+};
+use serde_json::json;
+
+use crate::{
+	MAX_PERMITTED_PASSWORD_LEN,
+	api::auth::models::LoginSchema,
+	database::{Database, LocalActor, tokens::TokenStore},
+	errors::{Context, Errcode, Error},
+};
+
+#[handler]
+pub(super) async fn login(
+	Json(payload): Json<LoginSchema>,
+	Data(db): Data<&Database>,
+	Data(token_store): Data<&TokenStore>,
+) -> Result<impl IntoResponse, Error> {
+	if payload.password.len() > MAX_PERMITTED_PASSWORD_LEN {
+		return Err(Error::new(
+			Errcode::IllegalInput,
+			Some(Context::new(
+				Some("password"),
+				Some(&format!("{} characters", payload.password.len())),
+				Some(&format!("Not more than {MAX_PERMITTED_PASSWORD_LEN} characters")),
+				None,
+			)),
+		));
+	}
+	let local_actor = match LocalActor::by_local_name(db, &payload.local_name).await? {
+		Some(actor) => actor,
+		None => return Err(Error::invalid_login()),
+	};
+	let actor_password_hashstring =
+		match LocalActor::get_password_hash(db, &payload.local_name).await? {
+			Some(hash_string) => hash_string,
+			None => {
+				return Err(Error::invalid_login());
+			}
+		};
+	let actor_password_hash = PasswordHash::new(&actor_password_hashstring).map_err(|e| {
+		error!(
+			"Password hash for user {} is not in PHC string format? Got error: {e}",
+			payload.password
+		);
+		Error::new(Errcode::Internal, None)
+	})?;
+	Argon2::default()
+		.verify_password(payload.password.as_bytes(), &actor_password_hash)
+		.map_err(|_| Error::invalid_login())?;
+	let token =
+		token_store.generate_upsert_token(&local_actor.unique_actor_identifier, None).await?;
+	Ok(Response::builder().status(StatusCode::OK).body(json!({"token": token}).to_string()))
+}
--- src/api/auth/mod.rs
+++ src/api/auth/mod.rs
@@ -2,11 +2,13 @@ use poem::{Route, post};
 
 /// The login endpoint
 mod login;
+/// Data models/schemas used for these routes
+pub(crate) mod models;
 /// The register endpoint
 mod register;
 
 #[cfg_attr(coverage_nightly, coverage(off))]
 /// Route handler for the auth module
 pub(super) fn setup_routes() -> Route {
-	Route::new().at("/register", post(register::register))
+	Route::new().at("/register", post(register::register)).at("/login", post(login::login))
 }
