cc5445358afe2df25df19f7dcdba9af45e50bc65
bitfl0wer <florian@pro-weber.com>
2025-07-19 19:06:39 +0200

    feat: finish Issuer::create_own

--- src/database/issuer.rs
+++ src/database/issuer.rs
@@ -1,5 +1,7 @@
 use chrono::NaiveDateTime;
+use log::error;
 use polyproto::{certs::idcert::IdCert, types::DomainName};
+use sqlx::query;
 
 use crate::{
 	config::SonataConfig,
@@ -11,18 +13,12 @@ use crate::{
 pub(crate) struct Issuer {
 	pub(crate) id: i64,
 	pub(crate) domain_components: DomainName,
-	pub(crate) id_cert: IdCert<DigitalSignature, DigitalPublicKey>,
 }
 
 #[cfg_attr(coverage_nightly, coverage(off))]
 impl Issuer {
-	/// Create (insert) the issuer entry for this sonata instance. Returns
-	/// `None`, if there is already a valid issuer entry for the given point in
-	/// time.
-	pub(crate) async fn create_own(
-		db: &Database,
-		time: NaiveDateTime,
-	) -> Result<Option<Self>, Error> {
+	/// Create (insert) the issuer entry for this sonata instance.
+	pub(crate) async fn create_own(db: &Database) -> Result<Option<Self>, Error> {
 		let config_domain = &SonataConfig::get_or_panic().general.server_domain;
 		let domain_name = DomainName::new(&config_domain).map_err(|e| {
 			Error::new(
@@ -30,6 +26,31 @@ impl Issuer {
 				Some(Context::new(None, None, None, Some(&e.to_string()))),
 			)
 		})?;
-		todo!()
+		let domain_name_separated =
+			domain_name.to_string().split('.').map(|s| s.to_owned()).collect::<Vec<_>>();
+		let record = query!(
+			r#"
+			INSERT INTO issuers (domain_components)
+			VALUES ($1)
+			ON CONFLICT (domain_components) DO NOTHING
+			RETURNING id, domain_components
+		"#,
+			&domain_name_separated
+		)
+		.fetch_optional(&db.pool)
+		.await?;
+		match record {
+			Some(row) => Ok(Some(Issuer {
+				id: row.id,
+				domain_components: match DomainName::new(&row.domain_components.join(".")) {
+					Err(e) => {
+						error!("Error: Invalid DomainName stored in issuers table: {e}");
+						return Err(Error::new_internal_error(None));
+					}
+					Ok(dn) => dn,
+				},
+			})),
+			None => Ok(None),
+		}
 	}
 }
